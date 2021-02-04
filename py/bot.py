f739318527fb792bdb743a91ed0c79dc96efb4aa
Sahil Pattni <sahilpattni97@gmail.com>
2021-02-05 00:40:34 +0400

    Added DialogGPT

--- py/bot.py
+++ py/bot.py
@@ -9,10 +9,15 @@ import hmac # Used for signed requests
 import os # Environment variables
 import re # Regex
 import binance
-from reactions import emoji_map
+from reactions import custom_emoji_map, default_emoji_map, phrase_map
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
 
 # Environment with access tokens
 env = os.environ
+
+
 
 # Global ID vars
 LOG_CHANNEL_NAME = 'bot_logs' # Channel for bot log output
@@ -21,6 +26,14 @@ VINAYAK = 713761220154621992 # Vinayak's ID
 SAHIL = 346393129915777034 # My ID
 BANNED_CHANNELS = ['trivia', 'chess', 'nsfw'] # Channels to ignore
 DEBUG_GUILD = 806913959260323848 
+CONVO_CHANNEL = 806977707555946527
+DEBUG_CONVO = 806985216714997770
+
+
+# Counting convo steps
+steps = 0
+chat_history = None
+tokenizer, model = None, None
 
 # Modules
 bot = commands.Bot(command_prefix='$')
@@ -35,11 +48,18 @@ async def log(message, guild):
 # On initialization
 @bot.event
 async def on_ready():
+    global tokenizer, model
     print('Connected and running...')
+    # Load up language model
+    print('Importing model....')
+    tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-large")
+    model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-large")
+    print('Model ready!')
 
 # Handle messages
 @bot.event
 async def on_message(message):
+    global steps, chat_history, tokenizer, model
     # Ignore these channels
     if message.channel.name.lower() in BANNED_CHANNELS:
         return
@@ -47,13 +67,47 @@ async def on_message(message):
     # Avoid bot responding to itself
     if message.author == bot.user:
         return
+    
+    if message.channel.id == CONVO_CHANNEL or message.channel.id == DEBUG_CONVO:
+        # Chat with Microsoft DialogGPT
+        new_ids = tokenizer.encode(message.content + tokenizer.eos_token, return_tensors='pt')
 
+        # Reset context every 5 steps
+        if steps == 0 or steps % 5 == 0:
+            chat_history = None
+        
+        # Generate bot tokens 
+        bot_ids = new_ids if chat_history is None else torch.cat([chat_history, new_ids], dim=-1)
+        # Update chat history and increment steps
+        chat_history = model.generate(bot_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+        steps += 1
+
+        # Prepare reply
+        response = tokenizer.decode(chat_history[:, bot_ids.shape[1]:][0], skip_special_tokens=True)
+
+        await message.channel.send(response)
+        return
+
+
+
+    message_words = message.content.lower().split(' ')
     # Iterate through words and check for matches
-    for word in emoji_map:
-        if word in message.content.lower():
-            reaction = emoji_map[word]
+    for word in custom_emoji_map:
+        if word in message_words:
+            reaction = custom_emoji_map[word]
             await message.add_reaction(discord.utils.get(bot.emojis, name=reaction))
     # Handle commands
+
+    for word in default_emoji_map:
+        if word in message_words:
+            reaction = default_emoji_map[word]
+            await message.add_reaction(reaction)
+    
+    for word in phrase_map:
+        if word in message.content.lower():
+            reaction = phrase_map[word]
+            await message.add_reaction(reaction)
+
     else:
         await bot.process_commands(message)
 
@@ -61,10 +115,15 @@ async def on_message(message):
 # Report deleted messages 
 @bot.event
 async def on_message_delete(message):
+    # Ignore these channels
+    if message.channel.name.lower() in BANNED_CHANNELS:
+        return
     # avoid recursive deletion messages
     if message.author == bot.user:
         return
-    await message.channel.send(f'{message.author} deleted the following message:\n"{message.content}"')
+    await message.channel.send(f'The following message was deleted:\n{message.author}: "{message.content}"')
+
+
 
 
 
