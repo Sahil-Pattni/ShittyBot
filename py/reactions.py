9fe0c3d9562d320bde7df13067145170484fea20
Sahil Pattni <sahilpattni97@gmail.com>
2021-02-05 00:40:12 +0400

    Added reaction map

--- py/reactions.py
+++ py/reactions.py
@@ -1,5 +1,13 @@
-emoji_map = {
+custom_emoji_map = {
     'bro': 'sike',
     'jesus': 'jesus',
-    'damn': 'damn'
+    'damn': 'damn',
+}
+
+default_emoji_map = {
+    'liberal': '\U0001F4A9'
+}
+
+phrase_map = {
+    'black people': '\U0001F349',
 }
