0359c4b4c19f25773a102922efe328207e985af3
Sahil Pattni <sahilpattni97@gmail.com>
2021-02-04 19:08:37 +0400

    Binance module to return all trades

--- py/binance.py
+++ py/binance.py
@@ -15,7 +15,7 @@ BASE = 'https://api.binance.com'
 
 
 # Generate hash-signed request
-def __signed_request(endpoint, additional_params=None):
+def __signed_request(endpoint, additional_params=None) -> dict:
     # Timestamp preliminary for request params
     timestamp = int(requests.get(f'{BASE}/api/v1/time').json()['serverTime'])
 
@@ -47,7 +47,7 @@ def __signed_request(endpoint, additional_params=None):
 
 
 # Returns the USDT equivalent of a crypto amount
-def __convert_to_USDT(ticker, amount):
+def __convert_to_USDT(ticker, amount) -> float:
     # If already USDT, don't convert
     if ticker == 'USDT':
         return amount
@@ -82,10 +82,6 @@ def __convert_to_USDT(ticker, amount):
         # Convert from TICKER to BNB to USDT, adjust for amount
         return amount * float(bnb_rate['price']) * float(usdt_bnb['price'])
 
-
-
-        
-
     # Get converted rate if no error. Float wrapped just in case
     return float(conv_rate['price']) * amount
         
@@ -93,7 +89,7 @@ def __convert_to_USDT(ticker, amount):
 
 
 # Get account balances
-def get_balances():
+def get_balances() -> tuple[list, list]:
     # Endpoint for account info
     url = f'{BASE}/api/v3/account'
     # Request resulting json
@@ -135,3 +131,39 @@ def get_balances():
     # Return both lists, sorted
     return free_assets, locked_assets
 
+
+# Gets all trades for given ticker
+def get_trades(ticker) -> dict:
+    url = f'{BASE}/api/v3/allOrders'
+    params = {'symbol': ticker}
+    result = __signed_request(url, additional_params=params)
+
+    # Handle error
+    if ['code'] in result:
+        raise ApiError(result['code'])
+
+    return result
+    
+
+def profit():
+    # Get tradable symbols
+    symbols = requests.get(f'{BASE}/api/v3/exchangeInfo').json()['symbols']
+
+    # Format: [FROM_TICKER, TO_TICKER, QTY, PRICE, TIMESTAMP]
+    all_trades = []
+
+    # Iterate through all pairs and add to trades if available
+    for symb in symbols:
+        from_ticker, to_ticker = symb['baseAsset'], symb['quoteAsset']
+        trades = get_trades(symb)
+
+        for trade in trades:
+            all_trades.append([
+                from_ticker,
+                to_ticker,
+                trade['executedQty'],
+                trade['price'],
+                trade['time']
+            ])
+    
+    return all_trades
