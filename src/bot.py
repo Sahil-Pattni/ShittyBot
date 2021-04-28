a648716d21c39590d09b7cc79e4a98a50b8861a9
Sahil Pattni <sahilpattni97@gmail.com>
2021-04-28 19:57:17 +0400

    Re-created reply method

--- src/bot.py
+++ src/bot.py
@@ -3,7 +3,8 @@ import discord
 import pickle
 import os
 from urllib.parse import urlparse
-#from predictor import predict
+from predictor import predict
+import numpy as np
 
 
 # Setup
@@ -16,6 +17,11 @@ GENERAL_CHANNEL = 747524804109664329
 POLITICAL_CHANEL = 806624699635335208 # Real one
 #POLITICAL_CHANEL = 805449044197507082 # bot_logs
 guild, channel, politics = None, None, None
+
+CATEGORIES = ['conservative', 'liberal', 'neutral']
+LIMIT = 0.8
+
+WELCOME_MESSAGE = 'Guess who\'s back bitches? I\'ve been trained on 100,000 Reddit comments on an LSTM with ~3.3 million trainable parameters. Keep sending me messages, and if I detect a statement that leans on either political extreme, I\' be sure to let you know!'
 
 def remove_links(text):
     text = text.strip()
@@ -45,21 +51,27 @@ async def on_ready():
     politics = discord.utils.get(guild.channels, id=POLITICAL_CHANEL)
 
     limit = 1000
-    await get_chat_history(politics, f'/Users/sloth_mini/Documents/Discord_Bot/data/message_log_{politics.name}.pkl', limit)
+    await channel.send(WELCOME_MESSAGE)
+    # await get_chat_history(politics, f'/Users/sloth_mini/Documents/Discord_Bot/data/message_log_{politics.name}.pkl', limit)
 
-# @bot.event
-# async def on_message(message):
-#     # Avoid bot responding to itself
-#     if message.author == bot.user:
-#         return
-#     if message.channel.id == GENERAL_CHANNEL:
-#         pred, _scores = predict(message.content, limit=0.93)
-#         if pred == '<NA>':
-#             print(f'{message.content}\n{_scores}\n')
-#         else:
-#             await message.reply(pred)
+@bot.event
+async def on_message(message):
+    # Avoid bot responding to itself
+    if message.author == bot.user:
+        return
+    if message.channel.id in [GENERAL_CHANNEL, POLITICAL_CHANEL]:
+        scores = predict(message.content)
+        if scores is not None:
+            value = max(scores)
+            if value < LIMIT:
+                return
+            tag = CATEGORIES[np.argmax(scores)]
+            reply = f'I am {value*100:,.2f}% sure this is a {tag} statement.'
+            await message.reply(reply)
 
-# ------------------------------------ #
+
+
+#------------------------------------ #
 if __name__ == '__main__':
     print('Running main method....')
     bot.run(env.get('TOKEN'))
