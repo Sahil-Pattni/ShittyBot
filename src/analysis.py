faa07e4fc5204a6b1037c3d692d0e2ba54da6d4a
Sahil Pattni <sahilpattni97@gmail.com>
2021-04-28 19:57:05 +0400

    Updated analyis file

--- src/analysis.py
+++ src/analysis.py
@@ -21,14 +21,18 @@ tagged = {
     'NEUTRAL': []
 }
 
+LIMIT = 0.8
+
 for c in comments:
     if len(c.split(' ')) < 3:
         continue
     try:
         scores = predict(c)
         if scores is not None:
+            value = max(scores)
             tag = categs[np.argmax(scores)]
-            tagged[tag].append(c)
+            if value >= LIMIT:
+                tagged[tag].append(c)
     except Exception as e:
         print(f'opps @ "{c}"')
 
@@ -37,6 +41,4 @@ print(f'Finished in {(time.time() - start)/60:,.2f} minutes')
 # %%
 for key, val in tagged.items():
     print(f'{key}: {len(val)}')
-# %%
-import matplotlib.pyplot as plt
 # %%
